95e6228b95ca76e03548dfba8126536507800b03
Andre Custodio <andre.custodio@itixti.com.br>
2018-12-09 16:33:53 -0200

    Criação de Abstração e Interface de AppService

---
+++ ArchSample.Application.Base/Abstractions/AppService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using ArchSample.Application.Base.Interfaces;
+using ArchSample.Domain.Base.Abstractions;
+
+namespace ArchSample.Application.Base.Abstractions
+{
+    public abstract class AppService<TEntity> : IAppService
+    {
+    }
+}
---
+++ ArchSample.Application.Base/Interfaces/IAppService.cs
@@ -0,0 +1,9 @@
+﻿using ArchSample.Domain.Base.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArchSample.Application.Base.Interfaces
+{
+    internal interface IAppService { }
+}
